85d8cd462b95c980c6cbff7cac8ce561685a6b1b
Shota Nakano <shotanakano62@gmail.com>
2024-07-26 16:39:02 +0900

    Create Chat, Detail, ChatList, UserInfo components

---
+++ src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,20 @@
+import './chatList.css'
+import { useState } from 'react'
+
+const ChatList = () => {
+  const [addMode, setAddMode] = useState(false)
+  return (
+    <div className='chatList'>
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder='Search' />
+        </div>
+        <img src={addMode ? "./minus.png" : "./plus.png"} alt="" className='add'
+        onClick={()=>setAddMode((prev) => !prev)}/>
+      </div>
+    </div>
+  )
+}
+
+export default ChatList
